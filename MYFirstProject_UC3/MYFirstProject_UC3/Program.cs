d1257281019f8cf78c6b4dbb5066a9953cddfa58
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-04-26 19:31:51 +0530

    Added: LP3

---
+++ MYFirstProject_UC3/MYFirstProject_UC3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYFirstProject_UC3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empwage = 0;
+            Random obj = new Random();
+            int empCheck = obj.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
+            {
+                emphrs = 8;
+
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                emphrs = 4;
+
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("The Employee wage of the day is : " + empwage);
+            Console.ReadKey();
+        }
+    }
+}
--- MYFirstProject_UC3/MYFirstProject_UC3/Properties/AssemblyInfo.cs
+++ MYFirstProject_UC3/MYFirstProject_UC3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MYFirstProject_UC2")]
+[assembly: AssemblyTitle("MYFirstProject_UC3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MYFirstProject_UC2")]
+[assembly: AssemblyProduct("MYFirstProject_UC3")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e0eb4b2b-fb15-4638-b8b9-9e1dd840d7b7")]
+[assembly: Guid("5182e2f9-3a93-4277-8bdb-bc1e143fb96b")]
 
 // Version information for an assembly consists of the following four values:
 //
