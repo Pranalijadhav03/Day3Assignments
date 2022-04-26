19009558b44344b03ee881a492410a9117cf47c1
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-04-26 19:25:21 +0530

    Added: LP2

--- MYFirstProject_UC2/MYFirstProject_UC2/Program.cs
+++ MYFirstProject_UC2/MYFirstProject_UC2/Program.cs
@@ -10,6 +10,26 @@ namespace MYFirstProject_UC2
     {
         static void Main(string[] args)
         {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOURS = 20;
+            int empWage = 0;
+            int empHours = 0;
+            Random obj = new Random();
+            int empCheck = obj.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHours = 8;
+            }
+
+            else
+            {
+                empHours = 0;
+            }
+            empWage = empHours * EMP_RATE_PER_HOURS;
+
+            Console.WriteLine("The Employee wage of the day is : " + empWage);
+            Console.ReadKey();
+
         }
     }
 }
